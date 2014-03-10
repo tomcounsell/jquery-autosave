a96edc039aa6984aff0f415ca5e649ab6f80392f
Tom Counsell <dev@tomcounsell.com>
2014-03-10 13:15:04 +0000

    update to ajax done, fail, callbacks

--- src/jquery-autosave.js
+++ src/jquery-autosave.js
@@ -3,15 +3,15 @@ jQuery.fn.autosave = function(options){
   $.each(this,function(){
     var $this = $(this);
     var defaults = {
-      data:{},
-      method: "POST",
       url:    "/",
+      method: "POST",
       event:  "change",
-      debug:  false,
+      data:   {},
       type:   "html",
-      success:function(){},
-      error:  function(){},
-      before: function(){}
+      debug:  false,
+      before: function(){},
+      done:function(){},
+      fail:  function(){}
     };
     options = $.extend(defaults, options); //options overwrite defaults
     var inline_options = getDataAttributes(this);
@@ -19,26 +19,25 @@ jQuery.fn.autosave = function(options){
     
     $this.on(event,function(e){
       var $element = $(this);
+
+      if(options.before){
+        options.before.call(this); // call in the context of the element
+      }
+      
       var inline_options = getDataAttributes(this); //get latest inline options
       options = $.extend(options, inline_options); //inline options overwrite options
       var data = $.extend(options.data, inline_options) //include all inline options in data
-
-      if(options.before){
-        options.before.call(this,$element);
-      }
       
       if(!options.debug) { // Unless in Debug Mode
+        
         $.ajax({
           url:      options.url,
           type:     options.method,
+          cache:    false,
           data:     data,
-          dataType: options.type,
-          success: function(data){
-            options.success.call(data,$element);
-          },
-          error:function(error){
-            options.error.call(error,$element);
-          }
+          dataType: options.type
+        }).done(function(response_data){ options.done(data,$element);
+        }).fail(function(jqXHR, text_status){ options.done(data,$element);
         });
         return true;
         
